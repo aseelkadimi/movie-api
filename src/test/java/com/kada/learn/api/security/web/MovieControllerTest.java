55c315f86d056df372688c9bde2a4a0e487ab094
aseelkadimi <aseelkadimi@gmail.com>
2020-05-21 17:33:55 -0400

    test jenkinsfile step

--- src/test/java/com/kada/learn/api/security/web/MovieControllerTest.java
+++ src/test/java/com/kada/learn/api/security/web/MovieControllerTest.java
@@ -1,40 +1,40 @@
-//package com.kada.learn.api.security.web;
-//
-//import com.kada.learn.api.security.model.Movie;
-//import com.kada.learn.api.security.service.MovieService;
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.Mock;
-//import org.mockito.Mockito;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//
-//import java.util.Collections;
-//import java.util.List;
-//
-//@ExtendWith(MockitoExtension.class)
-//class MovieControllerTest {
-//
-//    @Mock
-//    private MovieService service;
-//
-//    private MovieController underTest;
-//
-//    @BeforeEach
-//    public void setUp(){
-//        underTest = new MovieController(service);
-//    }
-//
-//    @Test
-//    public void testGetMovieWhenSuccess(){
-//        List<Movie> expectedMovies = Collections.singletonList(new Movie());
-//        Mockito.when(service.findAll()).thenReturn(expectedMovies);
-//
-//        ResponseEntity<?> actual = underTest.getMovies();
-//
-//        Assertions.assertEquals(HttpStatus.OK, actual.getStatusCode());
-//    }
-//}
+package com.kada.learn.api.security.web;
+
+import com.kada.learn.api.security.model.Movie;
+import com.kada.learn.api.security.service.MovieService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Collections;
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
+class MovieControllerTest {
+
+    @Mock
+    private MovieService service;
+
+    private MovieController underTest;
+
+    @BeforeEach
+    public void setUp(){
+        underTest = new MovieController(service);
+    }
+
+    @Test
+    public void testGetMovieWhenSuccess(){
+        List<Movie> expectedMovies = Collections.singletonList(new Movie());
+        Mockito.when(service.findAll()).thenReturn(expectedMovies);
+
+        ResponseEntity<?> actual = underTest.getMovies();
+
+        Assertions.assertEquals(HttpStatus.OK, actual.getStatusCode());
+    }
+}
